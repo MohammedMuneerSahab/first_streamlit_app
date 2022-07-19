045e002609f46dfe10ddfb443400027d9c834c35
THEMOHAMMEDMUNEER <105619521+MohammedMuneerSahab@users.noreply.github.com>
2022-07-19 14:54:02 +0530

    Update streamlit_app.py
    Added new command of st

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
-import streamlit
-streamlit.title("My parents new healthy Diner")
+import streamlit as st
+st.header('This is a header')
+st.subheader('This is a subheader')
+st.title("My parents new healthy Diner")
+st.text('This is some text.')
+st.text('This is some text.')
+st.text('This is some text.')
