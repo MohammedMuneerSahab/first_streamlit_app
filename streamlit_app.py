05852db0f81628bdc1b414c6a36fe00b7966f3d5
THEMOHAMMEDMUNEER <105619521+MohammedMuneerSahab@users.noreply.github.com>
2022-07-19 14:54:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit as st
+st.title("My parents new healthy Diner")
 st.header('This is a header')
 st.subheader('This is a subheader')
-st.title("My parents new healthy Diner")
+
 st.text('This is some text.')
 st.text('This is some text.')
 st.text('This is some text.')
