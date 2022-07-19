504fbd80a94f0fa2c697aa4cb4829f3a9ed88e43
THEMOHAMMEDMUNEER <105619521+MohammedMuneerSahab@users.noreply.github.com>
2022-07-19 14:59:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,9 @@
-import streamlit as st
+import streamlit 
 st.title("My parents new healthy Diner")
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
-st.header('This is a header')
-st.subheader('This is a subheader')
+
 
 
