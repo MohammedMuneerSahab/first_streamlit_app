0255b3a905570e26d2336adc99e551b63883cfd7
THEMOHAMMEDMUNEER <105619521+MohammedMuneerSahab@users.noreply.github.com>
2022-07-19 15:03:02 +0530

    Update streamlit_app.py
    Added a new header

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,6 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Free-Range Egg')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
